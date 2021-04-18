93229d888f89d81fc000bf8b14857f5ee9414c7d
shepherdingelectrons <dr.de.scott@gmail.com>
2021-04-18 20:27:15 +0100

    Python script to generate ROM for ALU EEPROM

---
+++ ALU/ALU_rom.py
@@ -0,0 +1,50 @@
+ROMsize=32 # 16 instructions * 2 Carry-flag states = 32
+
+ROM = bytearray([0x00]*ROMsize)
+
+def AddCommand(instruction, control, mode, Cin):
+    # address is comprised of:
+    # FLAG-C, I3, I2, I1, I0
+    #
+    # Cin = 3 means use Cin = NOT(FLAG-Carry)
+    # Cin = 2 means use Cin = FLAG-Carry
+    # Cin = 1 means Cin = HIGH
+    # Cin = 0 means Cin = 0
+
+    addr_CL = instruction & 0x0F
+    addr_CH = (instruction & 0x0F) | (1<<4)
+
+    output = (control & 0x0F) | (mode<<4)
+    if Cin==2:
+        ROM[addr_CL] = output | (0<<5)
+        ROM[addr_CH] = output | (1<<5)
+    elif Cin==3:
+        ROM[addr_CL] = output | (1<<5)
+        ROM[addr_CH] = output | (0<<5)
+    else:
+        ROM[addr_CL] = output | (Cin<<5)
+        ROM[addr_CH] = output | (Cin<<5)
+        
+AddCommand(0, 0b0000, 1, 1) # NOT
+AddCommand(1, 0b0001, 1, 1) # NOR int
+AddCommand(2, 0b0100, 1, 1) # NAND int
+AddCommand(3, 0b0101, 1, 1) # NOT int
+AddCommand(4, 0b0110, 1, 1) # XOR int
+AddCommand(5, 0b0110, 0, 0) # SUB int
+AddCommand(6, 0b0110, 0, 3) # SUBC int
+AddCommand(7, 0b1001, 0, 1) # ADD int
+AddCommand(8, 0b1011, 1, 1) # AND int
+AddCommand(9, 0b1100, 0, 1) # SHL (A+A)
+AddCommand(10, 0b1110, 1, 1) # OR int
+AddCommand(11, 0b1111, 0, 1) # DEC
+AddCommand(12, 0b1100, 0, 2) # RCL = 2A+Flag_Carry
+AddCommand(13, 0b0011, 1, 1) # CLR (A=0)
+AddCommand(14, 0b1001, 0, 2) # ADDC int
+AddCommand(15, 0b0110, 0, 0) # CMP int
+
+for i in range(0,16):
+	print(i,bin(ROM[i]),bin(ROM[i+16]))
+
+f = open("ALU.py","w")
+f.write("ROM="+str(ROM)+"\n")
+f.close()
