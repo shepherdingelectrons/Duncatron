e45a04bbbe4e2a5638ef2314432fbd60b52cf39e
shepherdingelectrons <shepherdingelectrons@users.noreply.github.com>
2025-06-27 15:22:48 +0100

    added ECHO subroutine

--- Duncatron C  compiler and ASM/asm files/loadtest.asm
+++ Duncatron C  compiler and ASM/asm files/loadtest.asm
@@ -19,6 +19,30 @@ dec A
 jnz myfunction
 
 pop T
-RET
+RET		; function is 26 bytes
+
+; Therefore this should start at 0x831a
+echo_function:
+	mov U,0x0A
+	mov U,0x0D
+	
+	mov r0,[0x0E]	; source string in r0r1
+	mov r1,[0x0F]
+	
+	display_string: ; from r0r1 to r2r3 until zero character
+	mov A,[r0r1]
+	mov U,A
+	cmp A,0x00
+	jz display_string.exit
+	inc r0r1
+	inc r2r3
+	jmp display_string
+	
+	display_string.exit:
+	mov U,0x0A
+	mov U,0x0D
+	
+	pop T
+	ret
 
 
