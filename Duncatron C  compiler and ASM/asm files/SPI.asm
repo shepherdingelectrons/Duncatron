42225f34336abb3a6d44e99fdb1f64e5d711c646
shepherdingelectrons <dr.de.scott@gmail.com>
2025-12-13 17:02:30 +0000

    Added MAX7219 support

--- Duncatron C  compiler and ASM/asm files/SPI.asm
+++ Duncatron C  compiler and ASM/asm files/SPI.asm
@@ -8,8 +8,8 @@ IN_PORT equ [0x8107]
 
 DS1302_SS equ 0x00  ; real-time control chip
 SD_SS equ 0x01      ; SD card
-FLASH equ 0x02 	    ; AT25DF081A EEPROM 1Mb (needs Vcc and/or signal voltage shifting)
-MAX7219 equ 0x03
+FLASH_SS equ 0x02 	    ; AT25DF081A EEPROM 1Mb (needs Vcc and/or signal voltage shifting)
+MAX7219_SS equ 0x03
 
 DS1302_seconds equ 0x01	; These values are already reversed to account for the LSB first requirement
 DS1302_minutes equ 0x41	; of DS1302.  Most SPI devices are MSB first, which is what the hardware
@@ -24,6 +24,140 @@ DS1302_clkburst equ 0x7d
 
 DS1302_RAM0 equ 0x03 ; 0b1100 0000 
 
+;MAX7219
+MAX7219_DECODE equ 0x09                        
+MAX7219_INTENSITY equ 0x0a                       
+MAX7219_SCAN_LIMIT equ 0x0b                       
+MAX7219_SHUTDOWN equ 0x0c                      
+MAX7219_DISPLAY_TEST equ 0x0f                       
+
+MAX7219_INTENSITY_MIN equ 0x00                       
+MAX7219_INTENSITY_MAX equ 0x0f    
+
+max7219_loop:
+	push_pc+1
+	call max7219_init
+	
+	mov r3,0x00
+	mov r4r5,digit_sequence
+	
+	clock_loop:
+		mov A,F	;128 64 32 16 8  4   2  1
+		;F7  F6 SD RDY OF N  C  Z
+		and A,0x10 ; RX character is waiting
+		jnz clock_loop.exit
+		
+		push_pc+1
+		call DS1302.get_seconds	; uses r0,r2 and returns in r1
+		mov [0x8106],r1
+		
+		;mov A,r3 	;previous seconds
+		;cmp A,r1
+		;je clock_loop
+		
+		;mov r3,r1	; update seconds 
+		
+		mov r3,0x01
+		push r4
+		push r5
+	
+	set_digits:
+		mov A,[r4r5]		
+		mov r2,MAX7219_SS
+		mov r0,r3;0x01
+		mov r1,A
+		push_pc+1
+		call SPI.write_register
+		
+		inc r4r5
+		
+		mov r0r1,end_digit_sequence
+		mov A,r5
+		cmp A,r1
+		
+		mov A,r4
+		cmpc A,r0 ; 16-bit compare
+		jl set_digits_no_overflow
+		
+		mov r4r5,digit_sequence
+	
+	set_digits_no_overflow:
+		inc r3
+		mov A,r3
+		cmp A,0x05
+		jne set_digits
+	pop r5
+	pop r4
+	inc r4r5
+	
+	mov r0r1,end_digit_sequence
+	mov A,r5
+	cmp A,r1
+	
+	mov A,r4
+	cmpc A,r0 ; 16-bit compare
+	jl clock_loop
+	mov r4r5,digit_sequence
+	
+	jmp clock_loop
+		
+clock_loop.exit:	
+	mov A,U
+	pop T 
+	RET
+digit_sequence:
+db 0x40,0x20,0x10,0x08,0x04,0x02
+end_digit_sequence:
+ 
+max7219_init:
+	mov r2,MAX7219_SS
+	mov r0,MAX7219_SCAN_LIMIT
+	mov r1,0x07
+	push_pc+1
+	call SPI.write_register
+	
+	mov r0,MAX7219_DECODE
+	mov r1,0x00
+	push_pc+1
+	call SPI.write_register
+	
+	mov r0,MAX7219_SHUTDOWN
+	mov r1,0x01
+	push_pc+1
+	call SPI.write_register
+	
+	mov r0,MAX7219_DISPLAY_TEST
+	mov r1,0x00
+	push_pc+1
+	call SPI.write_register
+	
+	mov r0,MAX7219_INTENSITY
+	mov r1,MAX7219_INTENSITY_MAX
+	push_pc+1
+	call SPI.write_register
+	
+	pop T
+	RET
+	
+ds1302_loop:
+	push_pc+1
+	call DS1302.init
+	
+	sec_loop:
+		mov A,F	;128 64 32 16 8  4   2  1
+		;F7  F6 SD RDY OF N  C  Z
+		and A,0x10 ; RX character is waiting
+		jnz sec_loop.exit
+		
+		push_pc+1
+		call DS1302.get_seconds
+		mov [0x8106],r1
+		jmp sec_loop
+		
+sec_loop.exit:
+pop T
+RET
+
 SPI.send: 	; The reason for breaking out such a simple function is that currently
 			; nop instructions are required to ensure timings and that the SPI device has
 			; completed sending before we read/send again.
@@ -37,9 +171,10 @@ SPI.send: 	; The reason for breaking out such a simple function is that currentl
 
 ; DS1302 init 
 DS1302.init:
+	mov r2,DS1302_SS
 	mov r0,DS1302_seconds
 	push_pc+1
-	call DS1302.read_register	; returns seconds register into r1
+	call SPI.read_register	; returns seconds register into r1
 	
 	mov A,r1
 	and A,0xFE	; preserve seconds, clear clock halt in LSB if set (0b0000 0001 if CH set)
@@ -48,26 +183,28 @@ DS1302.init:
 	;mov r0,DS1302_seconds
 	;mov r1,0x00		; clear clock halt flag
 	push_pc+1
-	call DS1302.write_register	; r0 still set, r1 set
+	call SPI.write_register	; r0 still set, r1 set and r2
 	
 	pop T
 	RET
 
 DS1302.get_seconds:	; read seconds:
+	mov r2,DS1302_SS
 	mov r0,DS1302_seconds 
 	push_pc+1
-	call DS1302.read_register	
+	call SPI.read_register	
 	; r1 returns value
 	pop T
 	RET
 
-DS1302.write_register:
+SPI.write_register:
 ; write register:
 ; parameters:
 ; 	r0 = register to set
 ; 	r1 = value to set register to
+;	r2 = Slave Select number
 
-mov A,DS1302_SS		
+mov A,r2		
 or A,0x08	
 mov CONTROL_REG,A	; set slave select = 0, SPI_EN = 1
 
@@ -79,19 +216,19 @@ mov A,r1			; r1 is data byte
 push_pc+1
 call SPI.send
 
-mov A,DS1302_SS
-mov CONTROL_REG,A	; SPI_EN = 0
+mov CONTROL_REG,r2	; SPI_EN = 0
 pop T
 RET
 
-DS1302.read_register:
+SPI.read_register:
 ; read register:
 ; parameters:
 ; 	r0 = register to set
+;	r2 = Slave Select device
 ; output:
 ; 	r1 = return value of register
 
-mov A,DS1302_SS		
+mov A,r2		
 or A,0x08	
 mov CONTROL_REG,A	; set slave select = 0, SPI_EN = 1
 
@@ -105,8 +242,7 @@ push_pc+1
 call SPI.send
 
 mov r1,SPI_PORT		; get MISO_byte into r1
-mov A,DS1302_SS
-mov CONTROL_REG,A	; SPI_EN = 0
+mov CONTROL_REG,r2	; SPI_EN = 0
 
 pop T
 RET
